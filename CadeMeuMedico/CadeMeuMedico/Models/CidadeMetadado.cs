3128e064bf8ade9fa9b78695f3d39a2c47bd06cd
guilherme-stefano <guilhermenostefano@gmail.com>
2018-08-23 18:25:00 -0300

    added data anotations to entity classes

---
+++ CadeMeuMedico/CadeMeuMedico/Models/CidadeMetadado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(CidadeMetadado))]
+    public partial class Cidades { }
+    public class CidadeMetadado
+    {
+        [Required(ErrorMessage = "Obrigatório Informar a Cidade")]
+        [StringLength(80, ErrorMessage = "O nome deve possuir no mínimo 80 caracteres")]
+        public string Nome { get; set; }
+    }
+}
---
+++ CadeMeuMedico/CadeMeuMedico/Models/EspecialidadeMetadado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(EspecialidadeMetadado))]
+    public partial class Especialidades { }
+    public class EspecialidadeMetadado
+    {
+        [Required(ErrorMessage ="O campo nome é obrigatório")]
+        [StringLength(80, ErrorMessage ="O nome deve possuir no mínimo 80 caracteres")]
+        public string Nome { get; set; }
+    }
+}
